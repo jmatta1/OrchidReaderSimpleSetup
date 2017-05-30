3813f8f3740b2c9fb0d6f27da8997c73755586ff
James Till Matta <jamesmatta@gmail.com>
2017-05-30 17:32:09 -0400

    Better handling of files with broken buffer at end
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- orchid_reader_simple_setup.py
+++ orchid_reader_simple_setup.py
@@ -377,8 +377,13 @@ def get_file_header_data(fname):
     # open as binary
     in_file = open(fname, 'rb')
     # check for that strange buffer header at beginning of file bug
-    if ((size - FILE_HEADER_SIZE) % BUFFER_SIZE) != 0:
-        in_file.seek(8192)
+    if ((size - FILE_HEADER_SIZE) % BUFFER_SIZE) == 8192:
+        rawdata = in_file.read(4)
+        startInt =  struct.unpack("<I", rawdata[0:])
+        if startInt == 0xf0f0f0f0:
+            in_file.seek(8192)
+        else:
+            in_file.seek(0, 0)
     # read the first 164 bytes
     rawdata = in_file.read(164)
     in_file.close()
