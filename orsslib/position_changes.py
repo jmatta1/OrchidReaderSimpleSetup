d9140bce0311c5c0a66c71ec85676af37ed856b8
James Till Matta <jamesmatta@gmail.com>
2017-07-18 12:23:21 -0400

    Added new position for the MIF work change
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- orsslib/position_changes.py
+++ orsslib/position_changes.py
@@ -23,14 +23,22 @@ SURVEYING_PATTERNS = ["May10_2017_0002.dat", "May11_2017_0000.dat",
 MAGNET_SCAN_PATTERNS = ["Mar29_2017_moved_0000", "Mar24_2017_SecPos_0000"
                         "Mar24_2017_SecPos_0001"]
 
-# list of array positions for the time series runs
+# list of the file name patterns that match when the array was moved in order
+# for changes to the MIF box to be done
+MIF_WORK_PATTERNS = ["Jul11_2017_0000.dat", "Jul11_2017_0001.dat",
+                     "Jul11_2017_0002.dat", "Jul12_2017_0000.dat",
+                     "Jul13_2017_0000.dat", "Jul14_2017_0000.dat",
+                     "Jul18_2017_0000.dat"]
+
+# list of array positions for the time series runs, the last is currently
+# approximate
 EXCEPTION_DATA = [(142.0, 74.0), (234.0, 279.0), (165.0, -124.0),
-                  (142.0, 156.0)]
+                  (142.0, 156.0), (234.0, 279.0)]
 
 # list of names for the array positions
 EXCEPTION_NAME = ["Default", "PastRxWall", "CloseToColdSrcCtrlRoom",
-                  "MovedForBFieldMeasurements"]
+                  "MovedForBFieldMeasurements", "MifBoxWork"]
 
 # list of position exception patterns (deliberately sans the default)
 EXCEPTION_PATTERN = [PAST_RX_WALL_PATTERNS, SURVEYING_PATTERNS,
-                     MAGNET_SCAN_PATTERNS]
+                     MAGNET_SCAN_PATTERNS, MIF_WORK_PATTERNS]
