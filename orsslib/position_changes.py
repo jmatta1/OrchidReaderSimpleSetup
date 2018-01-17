fcbc262801b38cf620a0a6538ac06315fc17fb84
James Till Matta <jamesmatta@gmail.com>
2018-01-17 17:23:24 -0500

    Removed a piece of silly shrapnel
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- orsslib/position_changes.py
+++ orsslib/position_changes.py
@@ -1,8 +1,5 @@
 """File that contains the variables defining the various sets of runs where
 the array is in positions other than default"""
-
-import 
-
 ###############################################################################
 # information for breaking the batches up based on array position
 ###############################################################################
