cdc0168be7547f0b861551b0863f8864653795af
James Till Matta <jamesmatta@gmail.com>
2018-01-17 17:33:10 -0500

    Fixed file name globs
    Signed-off-by: James Till Matta <jamesmatta@gmail.com>

--- orsslib/position_changes.py
+++ orsslib/position_changes.py
@@ -5,34 +5,35 @@ the array is in positions other than default"""
 ###############################################################################
 # list of file name patterns that match when the system was positioned past the
 # Reactor wall next to the saphire array cooling setup
-PAST_RX_WALL_PATTERNS = ["Jan12_2017_pastRxWall_0000.*",
-                         "Jan13_2017_pastRxWall_0000.*"]
+PAST_RX_WALL_PATTERNS = ["*/Jan12_2017_pastRxWall_0000.*",
+                         "*/Jan13_2017_pastRxWall_0000.*"]
 
 # list of file name patterns that match when the system was positioned close to
 # the door that leads towards the cold source room, due to surveyors 'shooting'
 # the area for PROSPECT
-SURVEYING_PATTERNS = ["May10_2017_0002.dat.*", "May11_2017_0000.dat.*",
-                      "May12_2017_0000.dat.*", "May15_2017_0000.dat.*",
-                      "May16_2017_0000.dat.*", "May18_2017_0000.dat.*",
-                      "May19_2017_0000.dat.*", "May21_2017_0000.dat.*",
-                      "May22_2017_0000.dat.*", "May22_2017_0001.dat.*"]
+SURVEYING_PATTERNS = ["*/May10_2017_0002.dat.*", "*/May11_2017_0000.dat.*",
+                      "*/May12_2017_0000.dat.*", "*/May15_2017_0000.dat.*",
+                      "*/May16_2017_0000.dat.*", "*/May18_2017_0000.dat.*",
+                      "*/May19_2017_0000.dat.*", "*/May21_2017_0000.dat.*",
+                      "*/May22_2017_0000.dat.*", "*/May22_2017_0001.dat.*"]
 
 # list of the file name patterns that match when the array was moved in order
 # to facilitate scanning of the magnetic field in the area that PROSPECT will
 # occupy
-MAGNET_SCAN_PATTERNS = ["Mar29_2017_moved_0000.*", "Mar24_2017_SecPos_0000.*"
-                        "Mar24_2017_SecPos_0001.*"]
+MAGNET_SCAN_PATTERNS = ["*/Mar24_2017_SecPos_0000.*",
+                        "*/Mar24_2017_SecPos_0001.*",
+                        "*/Mar29_2017_moved_0000.*"]
 
 # list of the file name patterns that match when the array was moved in order
 # for changes to the MIF box to be done
-MIF_WORK_PATTERNS = ["Jul11_2017_0000.dat.*", "Jul11_2017_0001.dat.*",
-                     "Jul11_2017_0002.dat.*", "Jul12_2017_0000.dat.*",
-                     "Jul13_2017_0000.dat.*", "Jul14_2017_0000.dat.*",
-                     "Jul18_2017_0000.dat.*"]
+MIF_WORK_PATTERNS = ["*/Jul11_2017_0000.dat.*", "*/Jul11_2017_0001.dat.*",
+                     "*/Jul11_2017_0002.dat.*", "*/Jul12_2017_0000.dat.*",
+                     "*/Jul13_2017_0000.dat.*", "*/Jul14_2017_0000.dat.*",
+                     "*/Jul18_2017_0000.dat.*"]
 
-SECOND_MAGNET_SCAN_PATTERN = ["Aug15_2017_moved_0000.*"]
+SECOND_MAGNET_SCAN_PATTERN = ["*/Aug15_2017_moved_0000.*"]
 
-NEW_DEFAULT_PATTERNS = ['Nov??_2017.*', 'Dec??_2017.*', 'Jan??_2018.*']
+NEW_DEFAULT_PATTERNS = ['*/Nov??_2017.*', '*/Dec??_2017.*', '*/Jan??_2018.*']
 
 # list of array positions for the time series runs, the last is currently
 # approximate
